1a29e992a39b5473c5d493ef95a5ad746a1122c4
miraculeux <miraculeux@gmail.com>
2025-09-20 21:07:55 +0800

    Initial Commit

---
+++ DOF/DOFApp.swift
@@ -0,0 +1,17 @@
+//
+//  DOFApp.swift
+//  DOF
+//
+//  Created by Marvel Zhu on 2025/9/20.
+//
+
+import SwiftUI
+
+@main
+struct DOFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
